9962ad42b734fbd927b73b57a8d7c2ba6dcc2c2a
rakafantino <rakafantino@gmail.com>
2024-01-10 04:40:18 +0700

    Fix:: Adjust the height of the home page to be at least the height of the screen so the toggle sound on/off can be seen on smartphone

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -60,7 +60,7 @@ const Home = () => {
   const [planeScale, planePosition] = adjustPlaneForScreenSize();
 
   return (
-    <section className="w-full h-screen relative">
+    <section className="w-full h-screen min-h-screen relative">
       <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">{currentStage && <Homeinfo currentStage={currentStage} />}</div>
       <Canvas className={`w-full h-screen bg-transparent ${isRotating ? "cursor-grabbing" : "cursor-grab"}`} camera={{ near: 0.1, far: 1000 }}>
         <Suspense fallback={<Loader />}>
